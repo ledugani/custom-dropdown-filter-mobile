bebb6f75791fdb8827c1fbb71c65000764390f09
Tom Dugan <thomas@aadwebb.com>
2020-12-22 10:03:11 -0600

    forgot to merge

--- src/app/dropdown/index.jsx
+++ src/app/dropdown/index.jsx
@@ -3,6 +3,7 @@ import "./styles.css";
 
 export default function Dropdown({ options, prompt, value, onChange }) {
 	const [ open, setOpen ] = useState(false);
+	const [ query, setQuery ] = useState("");
 	const ref = useRef(null);
 
 	useEffect(() => {
@@ -14,20 +15,38 @@ export default function Dropdown({ options, prompt, value, onChange }) {
 		setOpen(e && e.target === ref.current)
 	}
 
+	function filter(options) {
+		return options.filter(
+			(option) =>
+				option[label].toLowerCase().indexOf(query.toLowerCase()) > -1
+		);
+	}
+
 	return <div className="dropdown">
 		<div className="control" onClick={() => setOpen(prev => !prev)}>
-			<div className="selected-value" ref={ref}>
-				{value ? value.name : prompt}
+			<div className="selected-value">
+				<input
+					type="text"
+					ref={ref}
+					placeholder={value ? value[label] : prompt}
+					value={value || query}
+					onChange={e => {
+						setQuery(e.target.value)
+						onChange(null)
+					}}
+					onClick={() => setOpen(prev => !prev)}
+				/>
 			</div>
 			<div className={`arrow ${open ? "open" : null}`}></div>
 		</div>
 		<div className={`options ${open ? "open" : null}`}>
-			{options.map((option) => (
+			{filter(options).map((option) => (
 				<div
 					className={`option ${
 						value === option ? "selected" : null
 					}`}
 				onClick={() => {
+					setQuery("");
 					onChange(option);
 					setOpen(false);
 				}}>{option.name}</div>
