8aacb324ce270969613f3b6f404d903791f7d41a
Tom Dugan <thomas@aadwebb.com>
2020-12-21 21:17:30 -0600

    list showing - no style

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Dropdown from "./dropdown";
+import countries from "../data/countries.json"
+
+export default function App() {
+	return <div style={{ width: 200 }}>
+		<Dropdown countries={countries} />
+	</div>;
+}
