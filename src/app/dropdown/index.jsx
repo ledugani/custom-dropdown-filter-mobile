efc438d410f3b2b4ec94a5db6852bd0f715b1d99
Tom Dugan <thomas@aadwebb.com>
2020-12-22 09:38:15 -0600

    click outside box to close

--- src/app/dropdown/index.jsx
+++ src/app/dropdown/index.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./styles.css";
 
 export default function Dropdown({ options, prompt, value, onChange }) {
 	const [ open, setOpen ] = useState(false);
+	const ref = useRef(null);
+
+	useEffect(() => {
+		document.addEventListener("click", close);
+		return () => document.removeEventListener("click", close);
+	}, []);
+
+	function close(e) {
+		setOpen(e && e.target === ref.current)
+	}
+
 	return <div className="dropdown">
 		<div className="control" onClick={() => setOpen(prev => !prev)}>
-			<div className="selected-value">
+			<div className="selected-value" ref={ref}>
 				{value ? value.name : prompt}
 			</div>
 			<div className={`arrow ${open ? "open" : null}`}></div>
